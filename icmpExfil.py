aec50c273be3f2cb2904791e6e77eea999b28a4e
R3vanchist <110637379+R3vanchist@users.noreply.github.com>
2023-03-14 22:13:44 -0400

    Update icmpExfil.py

--- icmpExfil.py
+++ icmpExfil.py
@@ -2,17 +2,19 @@ from scapy.all import *
 import argparse
 
 
-parser = argparse.ArgumentParser(description="PCAP Dissector for Data Exfiltration")
+""" parser = argparse.ArgumentParser(description="PCAP Dissector for Data Exfiltration")
 parser.add_argument("-f", "--file", required=True, help="PCAP file to analyze")
-parser.add_argument("-s", "--source", required=True, help="Source IP address")
-parser.add_argument("-d", "--destination", required=True, help="Destination IP address")
 args = parser.parse_args()
 
 
-pcap = rdpcap(args.file)
-for packet in pcap:
-    if packet.haslayer(ICMP) and packet.haslayer(Raw):
-        payload = packet[Raw].load
-        payload = payload.decode("ascii")
-        if packet[IP].src == args.source and packet[IP].dst == args.destination:
-            print(f"Payload: {payload}")
+packets = rdpcap(args.file) """
+
+def icmp_exfil(packets):
+    for packet in packets:
+        try:
+            if packet.haslayer(ICMP) and packet.haslayer(Raw):
+                payload = packet[Raw].load
+                payload = payload.decode("ascii")
+                print(f"Payload: {payload}")
+        except IndexError:
+            continue
