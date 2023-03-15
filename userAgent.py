3ef1e4710f77bca533b9ed4731ad0cc202881cb6
R3vanchist <110637379+R3vanchist@users.noreply.github.com>
2023-03-14 22:22:19 -0400

    Update userAgent.py

--- userAgent.py
+++ userAgent.py
@@ -2,13 +2,13 @@ from scapy.all import *
 from scapy.layers.http import HTTPRequest
 import argparse
 
-# Pass the pcap as an argument
+""" # Pass the pcap as an argument
 parser = argparse.ArgumentParser(description="Displays user agents")
 parser.add_argument("-f", "--file", required=True, help="PCAP file to analyze")
 args = parser.parse_args()
 
 # Read the pcap file
-packets = rdpcap(args.file)
+packets = rdpcap(args.file) """
 
 # Extract user agents
 def extract_user_agents(packets):
@@ -18,11 +18,7 @@ def extract_user_agents(packets):
             user_agent = packet[HTTPRequest].User_Agent
             if user_agent not in user_agents:
                 user_agents.append(user_agent)
-    return user_agents
+    for user_agent in user_agents:
+        print(f'User Agents: {user_agents}')
 
-# Call the function and print the results
-user_agents = extract_user_agents(packets)
-print("User Agents:")
-for user_agent in user_agents:
-    print(user_agent)
     
